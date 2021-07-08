a33d7d8593683bc328662b4de5732019613c192d
csaba hullar <csaba.hullar@capture.eu>
2021-07-08 11:19:43 +0200

    Base project added to repository.

---
+++ demo-for-openshift/src/main/java/eu/hucsu/demoforopenshift/DemoForOpenshiftApplication.java
@@ -0,0 +1,21 @@
+package eu.hucsu.demoforopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoForOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForOpenshiftApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "OpenShift") String name) {
+		return String.format("Hello %s!", name);
+	}
+}

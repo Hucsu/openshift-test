b69057a91c09f2631c8ddedcafabbc89dadbbb7f
csaba hullar <csaba.hullar@capture.eu>
2021-07-12 14:57:47 +0200

    Add hello name for the RestController.

--- demo-for-openshift/src/main/java/eu/hucsu/demoforopenshift/DemoForOpenshiftApplication.java
+++ demo-for-openshift/src/main/java/eu/hucsu/demoforopenshift/DemoForOpenshiftApplication.java
@@ -1,5 +1,7 @@
 package eu.hucsu.demoforopenshift;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoForOpenshiftApplication {
 
+	Logger log = LoggerFactory.getLogger(DemoForOpenshiftApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(DemoForOpenshiftApplication.class, args);
 	}
 
-	@GetMapping("/")
+
+
+	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "OpenShift") String name) {
+		log.info("Name: "+name);
 		return String.format("Hello %s!", name);
 	}
 }
